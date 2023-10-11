cc455488465308e2769e681e9dcd8b3b3b5d9c7c
rundweb <124647801+rundweb@users.noreply.github.com>
2023-10-11 10:39:59 +0700

    Add files via upload

---
+++ dashboard/main/main.js
@@ -0,0 +1,109 @@
+var options = {
+  chart: {
+    height: 280,
+    type: "area",
+    toolbar:{
+        show: false,
+    }
+  },
+  series: [
+    {
+      name: "sales",
+      data: [0, 10, 20, 20, 30, 20],
+    },
+  ],
+  xaxis: {
+    categories: ["08:00", "09:00", "12:00", "15:00", "18:00", "21:00"],
+  },
+  dataLabels:{
+    enabled: true,
+    style:{
+        fontSize : '10px',
+    },
+  },
+};
+
+var chart = new ApexCharts(
+  document.querySelector("#content-line-chart"),
+  options
+);
+
+chart.render();
+
+var options2 = {
+  series: [
+    {
+      name: "Covid",
+      data: [28, 29, 33, 36],
+    },
+    {
+      name: "BPJS",
+      data: [12, 11, 14, 18],
+    },
+    {
+      name: "Asuransi Lain",
+      data: [14, 22, 33, 22],
+    },
+    {
+      name: "Umum",
+      data: [22, 20, 19, 33],
+    },
+  ],
+  chart: {
+    height: 300,
+    type: "line",
+    dropShadow: {
+      enabled: true,
+      color: "#000",
+      top: 18,
+      left: 7,
+      blur: 10,
+      opacity: 0.2,
+    },
+    toolbar: {
+      show: false,
+    },
+  },
+  colors: ["#EB4755", "#FF722C","#0060A3", "#2DBE78"],
+  dataLabels: {
+    enabled: true,
+  },
+  stroke: {
+    curve: "smooth",
+  },
+
+  grid: {
+    borderColor: "#e7e7e7",
+    row: {
+      colors: ["#f3f3f3", "transparent"], // takes an array which will be repeated on columns
+      opacity: 0.5,
+    },
+  },
+  markers: {
+    size: 1,
+  },
+  dataLabels:{
+    enabled: true,
+    style:{
+        fontSize : '10px',
+    },
+  },
+  xaxis: {
+    categories: ["a1", "a2", "a3", "a4"],
+    
+  },
+
+  legend: {
+    position: 'bottom',
+    horizontalAlign: 'center',
+    itemMargin:{
+        horizontal:30,
+    }
+  },
+};
+
+var chart2 = new ApexCharts(
+  document.querySelector("#content-multiline-chart"),
+  options2
+);
+chart2.render();
